e8f0a852f83b5aa2e952f60fa91c0d12764b6f0b
Jean Kim <jeaneonkim@gmail.com>
2021-09-02 14:55:09 -0700

    added jean page

--- main.py
+++ main.py
@@ -31,15 +31,15 @@ def hawkers():
 def stub():
     return render_template("stub.html")
 
-@app.route('/greet', methods=['GET', 'POST'])
-def greet():
+@app.route('/greet_jean', methods=['GET', 'POST'])
+def greet_jean():
     # submit button has been pushed
     if request.form:
         name = request.form.get("name")
         if len(name) != 0:  # input field has content
-            return render_template("greet.html", nickname=name)
+            return render_template("greet_jean.html", nickname=name)
     # starting and empty input default
-    return render_template("greet.html", nickname="World")
+    return render_template("greet_jean.html", nickname="World")
 
 # runs the application on the development server
 if __name__ == "__main__":
