177f355e010f1f04ef5497dd4e74f525fd5e27d3
xiaoa0 <xiaoallie@gmail.com>
2021-09-02 20:48:35 -0700

    Allie greet page

--- main.py
+++ main.py
@@ -31,15 +31,15 @@ def hawkers():
 def stub():
     return render_template("stub.html")
 
-@app.route('/greet', methods=['GET', 'POST'])
+@app.route('/allie_greet', methods=['GET', 'POST'])
 def greet():
     # submit button has been pushed
     if request.form:
         name = request.form.get("name")
         if len(name) != 0:  # input field has content
-            return render_template("greet.html", nickname=name)
+            return render_template("allie_greet.html", nickname=name)
     # starting and empty input default
-    return render_template("greet.html", nickname="World")
+    return render_template("allie_greet.html", nickname="World")
 
 # runs the application on the development server
 if __name__ == "__main__":
