4393dcedab1bf937455daf325824db4da1982ffe
Jean Kim <jeaneonkim@gmail.com>
2021-10-10 18:23:54 -0700

    Added Tech Talk Activity To IntelliJ

--- list.py
+++ list.py
@@ -1,2 +1,10 @@
 student_list=['pam','rob','joe','greg','bob','amy','matt']
+#Activity 1
 print(student_list[2:5])
+print(student_list[:-5])
+print(student_list[5:])
+print(student_list)
+if 'rob' in student_list:
+    print("Rob is part of the student_list")
+else:
+    print("Rob is not part of the student_list")
