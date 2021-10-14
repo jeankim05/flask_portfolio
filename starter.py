5c7f76f7de18cde3ce0e877dfa68016e6e1f64a9
Jean Kim <jeaneonkim@gmail.com>
2021-10-14 15:24:48 -0700

    Added all files from tech talk and updated navbar along with main and starter

--- starter.py
+++ starter.py
@@ -1,18 +1,68 @@
+import requests
 from flask import Blueprint, render_template
 from algorithm.image import image_data
 
-starter_bp = Blueprint('starter', __name__,
-                       url_prefix='/starter',
-                       template_folder='templates',
-                       static_folder='static',
-                       static_url_path='assets')
+from pathlib import \
+    Path  # https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+
+app_starter = Blueprint('starter', __name__,
+                        url_prefix='/starter',
+                        template_folder='templates',
+                        static_folder='static',
+                        static_url_path='assets')
 
 
-@starter_bp.route('/binary/')
+@app_starter.route('/binary/')
 def binary():
-    return render_template("starter/binary.html")
+    return render_template("binary.html")
 
 
-@starter_bp.route('/rgb/')
+@app_starter.route('/rgb/')
 def rgb():
-    return render_template('starter/rgb.html', images=image_data())
+    path = Path(app_starter.root_path) / "static" / "img"
+    return render_template('rgb.html', images=image_data(path))
+
+
+@app_starter.route('/joke', methods=['GET', 'POST'])
+def joke():
+    """
+    # use this url to test on and make modification on you own machine
+    url = "http://127.0.0.1:5222/api/joke"
+    """
+    url = "https://csp.nighthawkcodingsociety.com/api/joke"
+    response = requests.request("GET", url)
+    return render_template("joke.html", joke=response.json())
+
+
+@app_starter.route('/jokes', methods=['GET', 'POST'])
+def jokes():
+    """
+    # use this url to test on and make modification on you own machine
+    url = "http://127.0.0.1:5222/api/jokes"
+    """
+    url = "https://csp.nighthawkcodingsociety.com/api/jokes"
+
+    response = requests.request("GET", url)
+    return render_template("jokes.html", jokes=response.json())
+
+
+@app_starter.route('/covid19', methods=['GET', 'POST'])
+def covid19():
+    url = "https://corona-virus-world-and-india-data.p.rapidapi.com/api"
+    headers = {
+        'x-rapidapi-key': "dec069b877msh0d9d0827664078cp1a18fajsn2afac35ae063",
+        'x-rapidapi-host': "corona-virus-world-and-india-data.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+    """
+    # uncomment this code to test from terminal
+    world = response.json().get('world_total')
+    countries = response.json().get('countries_stat')
+    print(world['total_cases'])
+    for country in countries:
+        print(country["country_name"])
+    """
+
+    return render_template("covid19.html", stats=response.json())
