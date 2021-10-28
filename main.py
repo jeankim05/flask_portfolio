ad7ab82f823d93bf5b388f5a73f4e0577393ded8
Jean Kim <jeaneonkim@gmail.com>
2021-10-28 14:56:51 -0700

    Added a Sports Forum Page to PBL

--- main.py
+++ main.py
@@ -273,6 +273,16 @@ def sportsscore():
 def practice():
     return render_template("practice.html")
 
+@app.route('/sportsforum/', methods=['GET', 'POST'])
+def sportsforum():
+    # submit button has been pushed
+    if request.form:
+        name = request.form.get("name")
+        if len(name) != 0:  # input field has content
+            return render_template("sportsforum.html", greetforum=name)
+    # starting and empty input default
+    return render_template("sportsforum.html", greetforum="World")
+
 # runs the application on the development server
 if __name__ == "__main__":
     app.run(
